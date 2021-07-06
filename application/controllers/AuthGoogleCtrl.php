39c1a330e02d7906f75e036ba089a2ef5a9c4582
dragon1211 <vikt0r1211@hotmail.com>
2021-07-06 03:31:30 -0700

    modified oauth2callback() king in Jul 6

--- application/controllers/AuthGoogleCtrl.php
+++ application/controllers/AuthGoogleCtrl.php
@@ -10,46 +10,8 @@ class AuthGoogleCtrl extends CI_Controller {
     }
 
 	public function oauth2callback(){
-
         $this->session->set_userdata('logged_in', true); 
         $this->session->set_userdata('is_confirmed', false);
         redirect(base_url());
-        
-         /* Load google login view */
-        // $this->load->view('user_authentication/index',$data); 
-		// echo "success google login";
-
-		//  $google_data = $this->google->validate();
-	
-		// echo '<pre>'; print_r($this->$google); echo '</pre>';
-		
-		// 
-		// $user=$this->general_model->GetAllInfo('user','id',array('email'=>$google_data['email'],'source'=>'google'));
-		// if(count($user)>0){
-		// 	$session_data=array(
-		// 		'name'=>$google_data['name'],
-		// 		'email'=>$google_data['email'],
-		// 		'source'=>'google',
-		// 		'profile_pic'=>$google_data['profile_pic'],
-		// 		'link'=>$google_data['link'],
-		// 		'sess_logged_in'=>1
-		// 		);
-		// 	$this->session->set_userdata($session_data);
-			
-		// }else{
-			
-		// 	$session_data=array(
-		// 		'name'=>$google_data['name'],
-		// 		'email'=>$google_data['email'],
-		// 		'source'=>'google',
-		// 		'profile_pic'=>$google_data['profile_pic'],
-		// 		'link'=>$google_data['link'],
-		// 		'sess_logged_in'=>1
-		// 		);
-		// 	$this->session->set_userdata($session_data);
-		// }
-		
-		// redirect(base_url());
-
 	}
 }
